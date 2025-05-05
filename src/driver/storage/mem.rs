9e952ea2993aa1a7b02dd18e2d11ae1869af0377
carp-sushi <105142707+carp-sushi@users.noreply.github.com>
2025-05-05 13:56:48 -0600

    add memory storage error cases

--- src/driver/storage/mem.rs
+++ src/driver/storage/mem.rs
@@ -6,7 +6,8 @@ use std::sync::{Arc, RwLock};
 use uuid::Uuid;
 
 /// In-memory binary object storage.
-/// NOTE: This implementation of `Storage` panics, so it is only useful for testing.
+/// NOTE: This only allows a number of readers or at most one writer at any point in time.
+/// For this reason, it is only useful for testing or running in local a dev environment.
 type DataStore = Arc<RwLock<HashMap<Uuid, Vec<u8>>>>;
 
 /// Store binary objects in memory.
@@ -42,6 +43,8 @@ impl Storage<Uuid> for MemoryStorage {
         let key = Uuid::new_v4();
         if let Ok(mut map) = self.datastore.write() {
             map.insert(key, bytes.to_vec());
+        } else {
+            return Err(Error::internal("write lock fail".into()));
         }
         Ok(key)
     }
@@ -50,6 +53,8 @@ impl Storage<Uuid> for MemoryStorage {
     async fn delete(&self, key: Uuid) -> Result<()> {
         if let Ok(mut map) = self.datastore.write() {
             map.remove(&key);
+        } else {
+            return Err(Error::internal("write lock fail".into()));
         }
         Ok(())
     }
